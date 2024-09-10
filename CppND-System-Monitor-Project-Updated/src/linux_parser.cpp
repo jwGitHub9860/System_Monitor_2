ca4a0b0c7e518404570fb17281ce648999abc9a3
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-10 19:04:42 -0400

    Code for "linux_parser.cpp"

--- CppND-System-Monitor-Project-Updated/src/linux_parser.cpp
+++ CppND-System-Monitor-Project-Updated/src/linux_parser.cpp
@@ -9,6 +9,10 @@ using std::stof;
 using std::string;
 using std::to_string;
 using std::vector;
+using std::ifstream;
+using std::istringstream;
+
+//using namespace std;
 
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
@@ -67,49 +71,258 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization()
+{
+  float total_memory, free_memory;
+  string key, line;
+  float value;
+  ifstream stream(kProcDirectory + kMeminfoFilename);
+  if (stream.is_open())
+  {
+    while (getline(stream, line))  // read in line; while loop used due to multiple lines
+    {
+      istringstream linestream(line);    // creates string stream from "line"
+      linestream >> key >> value;  // allows to pull tokens off stream     1st token - key     2nd token - value
+      if (key == "MemTotal:")  // check if "line" holds cpu info (line = cpu / cpu...#)
+      {
+        total_memory = value;
+      }
+      else if (key == "MemFree:")
+      {
+        free_memory = value;
+      }
+    }
+  }
+  return (total_memory - free_memory) / (total_memory * 100);
+}
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() 
+{
+  long uptime;  // 1ST LARGE VALUE    { 1st value -> system uptime 2nd value -> system idle time} <---(in Cmake)
+  string line;
+  ifstream stream(kProcDirectory + kUptimeFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + version file name"
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);    // creates string stream from "line"
+    linestream >> uptime;  // allows to pull tokens off stream     1st token - uptime     2nd token - idle_time    78322.97 1119670.94 <---(in Cmake)
+  }
+  return uptime;  // if opening string or something else fails, return "uptime" as Blank String Default     MUST CHANGE "uptime" from 'string' to 'long' LAST or error will be returned
+}
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies()   // TOTAL JIFFIES CPU executed
+{
+  long jiffies;
+  vector<string> CPU_Info = CpuUtilization();   // inputs values into vector
+  for (auto i : CPU_Info)    // iterates through entire /proc/stat file
+  {
+    jiffies += stol(i);   // JIFFIES ARE THE LARGE NUMBERS
+  }
+  return jiffies;
+}
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
+long LinuxParser::ActiveJiffies(int pid)
+{
+  vector<string> jiffies;
+  string value, line;
+  ifstream stream(kProcDirectory + to_string(pid) + kStatFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + pid + stat file name"
+
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);    // creates string stream from "line"
+    for (int i = 0; i < 17; i++)
+    {
+      linestream >> value;  // allows to pull tokens off stream     first token - value
+      jiffies.emplace_back(value);
+    }
+    return stol(jiffies[16]) + stol(jiffies[15]) + stol(jiffies[14]) + stol(jiffies[13]);  // for '/proc/[PID]/stat', Active Jiffies = cstime + cutime + stime + utime    Active Jiffies = index[16] + index[15] + index[14] + index[13]
+  }
+  return 0;
+}
 
 // TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
+long LinuxParser::ActiveJiffies()
+{
+  vector<string> jiffies = CpuUtilization();
+  return stol(jiffies[kUser_] + jiffies[kNice_] + jiffies[kSystem_]);  // Active Jiffies = user + nice + system
+}
 
 // TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
+long LinuxParser::IdleJiffies()
+{
+  vector<string> jiffies = CpuUtilization();  // obtains "jiffies" from CpuUtilization() function
+  return stol(jiffies[kIdle_]) + stol(jiffies[kIOwait_]);  // Idle Jiffies = idle + iowait
+}
 
 // TODO: Read and return CPU utilization
-vector<string> LinuxParser::CpuUtilization() { return {}; }
+vector<string> LinuxParser::CpuUtilization()  // finds lines that hold cpu info & inputs values into vector
+{
+  vector<string> jiffies;   // holds jiffies (Large Values)
+  string line, cpu_key, value;
+  ifstream stream(kProcDirectory + kStatFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + stat file name"
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);    // creates string stream from "line"
+    linestream >> cpu_key;  // allows to pull tokens off stream     first token - cpu_key     checks if "cpu_key" holds cpu info (key = cpu / cpu...#)
+
+    while (linestream >> value)  // allows to pull tokens off stream     first token - value     pulls off numbers in cpu line, NOT cpu string; while loop used due to multiple lines
+    {
+      jiffies.emplace_back(value);
+    }
+  }
+  return {};
+}
 
 // TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses()
+{
+  int total_processes;
+  string line;
+  string key;
+  ifstream stream(kProcDirectory + kStatFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + stat file name"
+  if (stream.is_open())
+  {
+    while (getline(stream, line))   // gets line from stream & stores it in "string line"
+    {
+      istringstream linestream;  // input string stream
+      while (linestream >> key >> total_processes)   // gets line from stream & stores it in "string line"     first token - processes    second token - " # "    78322.97 1119670.94 <---(in Cmake)
+      {
+        if (key == "processes")   // checks if line contains total number of processes
+        {
+          return total_processes;
+        }
+      }
+    }
+  }
+  return total_processes;
+}
 
 // TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+int LinuxParser::RunningProcesses()
+{
+  int num_of_run_process;
+  string line;
+  string key;
+  ifstream stream(kProcDirectory + kStatFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + stat file name"
+  if (stream.is_open())
+  {
+    while (getline(stream, line))   // gets line from stream & stores it in "string line"
+    {
+      istringstream linestream(line);  // input string stream
+      while (linestream >> key >> num_of_run_process)   // allows to pull tokens off stream     first token - procs_running    second token - " # "    78322.97 1119670.94 <---(in Cmake)
+      {
+        if (key == "procs_running")   // checks if line contains number of processes running
+        {
+          return num_of_run_process;
+        }
+      }
+    }
+  }
+  return num_of_run_process;
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid)
+{
+  string cmdline;
+  ifstream stream(kProcDirectory + to_string(pid) + kCmdlineFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + pid + cmdline file name"
+  if (stream.is_open())
+  {
+    getline(stream, cmdline);   // gets cmdline from stream & stores it in "string line"
+    stream.close();
+    return cmdline;
+  }
+  
+  return string();
+}
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Ram(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Ram(int pid)
+{
+  string line, key, value;
+  ifstream stream(kProcDirectory + to_string(pid) + kStatusFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + pid + status file name"
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);  // input string stream
+    linestream >> key >> value;  // allows to pull tokens off stream     1st token - key     2nd token - value
+    if (key == "VmSize")   // checks if "key" contains 'VmSize' (memory usage)
+    {
+      return value;
+    }
+  }
+  return string();
+}
 
 // TODO: Read and return the user ID associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Uid(int pid)
+{
+  string line, key, value;
+  ifstream stream(kProcDirectory + to_string(pid) + kStatusFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + pid + status file name"
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);  // input string stream
+    linestream >> key >> value;  // allows to pull tokens off stream     1st token - key     2nd token - value
+    if (key == "Uid:")   // checks if "key" contains 'Uid' (user ID)
+    {
+      stream.close();
+      return value;
+    }
+  }
+  return string();
+}
 
 // TODO: Read and return the user associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::User(int pid)
+{
+  string line, user, letter_x, path;
+  string Uid = LinuxParser::Uid(pid);   // function that returns USER
+  ifstream stream(kPasswordPath);  // input file stream from path for operating system kernel version          operating system kernel version - "password path directory"
+  if (stream.is_open())
+  {
+    while (getline(stream, line))  // read in line; while loop used due to multiple lines
+    {
+      replace(line.begin(), line.end(), ':', ' ');
+      istringstream linestream(line);  // input string stream
+      while (linestream >> user >> letter_x >> path)  // allows to pull tokens off MULTIPLE streams     1st token - user     2nd token - letter_x     3rd token - path
+      {
+        if (user == Uid)  // checks if "user" matches "Uid"
+        {
+          return user;
+        } 
+      }
+    }
+  }
+  return string();
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::UpTime(int pid[[maybe_unused]]) { return 0; }
+long LinuxParser::UpTime(int pid)
+{
+  long uptime;
+  string line;
+  ifstream stream(kProcDirectory + to_string(pid) + kStatFilename);  // input file stream from path for operating system kernel version          operating system kernel version - "proc directory + pid + STAT file name"          use Same stream as "TotalProcesses()" & "RunningProcesses()" since both deal with processes
+  if (stream.is_open())
+  {
+    getline(stream, line);   // gets line from stream & stores it in "string line"
+    istringstream linestream(line);  // input string stream
+    for (int i = 0; i < 22; i++)  // iterates through all lines until 'line 21' holds "processes" line - number of PROCESSES & threads created
+    {
+      linestream >> uptime;  // allows to pull tokens off stream     1st token - uptime     2nd token - idle_time    78322.97 1119670.94 <---(in Cmake Example)
+    }
+    return uptime / sysconf(_SC_CLK_TCK);   // sysconf(_SC_CLK_TCK) - returns number of clock ticks per second, amount of time measured in USER_HZ
+  }
+  return uptime;  // if opening long int or something else fails, return "uptime" as Blank Long Int Default
+}
