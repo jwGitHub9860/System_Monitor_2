b54b3f1c7c126efdde62f92fd8b6a68c1b2e489c
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-10 19:25:19 -0400

    Code for "process.h"

--- CppND-System-Monitor-Project-Updated/include/process.h
+++ CppND-System-Monitor-Project-Updated/include/process.h
@@ -8,6 +8,7 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
+  Process(int pid);     // constructor ---> DEFINES SPECIFIC "int pid" OF SPECIFIC PROCESS
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -18,6 +19,21 @@ class Process {
 
   // TODO: Declare any necessary private members
  private:
+  int pid_;
+  
+  int process_ID;
+
+  float prev_total_jiffies = 0;  // MUST BE INITIALIZED
+  float prev_idle_jiffies = 0;  // MUST BE INITIALIZED
+  float prev_User = 0;  // MUST BE INITIALIZED
+  float prev_Nice = 0;  // MUST BE INITIALIZED
+  float prev_System = 0;  // MUST BE INITIALIZED
+  float prev_Idle = 0;  // MUST BE INITIALIZED
+  float prev_Iowait = 0;  // MUST BE INITIALIZED
+  float prev_Irq = 0;  // MUST BE INITIALIZED
+  float prev_Softirq = 0;  // MUST BE INITIALIZED
+
+  int ram_usage;
 };
 
 #endif
