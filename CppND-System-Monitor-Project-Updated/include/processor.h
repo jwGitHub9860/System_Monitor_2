6698a1edd1c3c100f8b543addf3e0e583522231d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-18 21:44:00 -0400

    Code for "system.cpp", "system.h", "processor.cpp", and "processor.h"

--- CppND-System-Monitor-Project-Updated/include/processor.h
+++ CppND-System-Monitor-Project-Updated/include/processor.h
@@ -1,12 +1,19 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+   float prev_total_time = 0;
+   float prev_active_time = 0;
 };
 
 #endif
--- CppND-System-Monitor-Project-Updated/src/processor.cpp
+++ CppND-System-Monitor-Project-Updated/src/processor.cpp
@@ -1,4 +1,26 @@
+#include <cmath>
+#include <iostream>
+
 #include "processor.h"
+#include "linux_parser.h"
+#include "process.h"
+
+using namespace std;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization()
+{
+    long total_jiffies = LinuxParser::Jiffies();
+    long active_jiffies = LinuxParser::ActiveJiffies();
+    
+    float current_total_time = total_jiffies;   // change long to float
+    float current_active_time = active_jiffies;   // change long to float
+
+    float total_time = current_total_time - prev_total_time;
+    float active_time = current_active_time - prev_active_time;
+
+    float prev_total_time = total_time;    // make Current total time, Previous total time
+    float prev_active_time = active_time;    // make Current active time, Previous active time
+    
+    return (active_time / total_time) * total_time;     // CPU usage percent = (Active_CPU_time / Total_CPU_time) * 100         total_time = 100%
+}
