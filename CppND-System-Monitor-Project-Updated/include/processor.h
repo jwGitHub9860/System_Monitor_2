ae217a96fe99b2d2868036d18e851b908bbab879
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-28 12:28:14 -0400

    Corrections for processor.cpp & processor.h

--- CppND-System-Monitor-Project-Updated/include/processor.h
+++ CppND-System-Monitor-Project-Updated/include/processor.h
@@ -12,8 +12,20 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
-   float prev_total_time = 0;
-   float prev_active_time = 0;
+   float Prev_Idle_Total = 0;  // MUST BE INITIALIZED
+   float Prev_Idle = 0;  // MUST BE INITIALIZED
+   float Prev_Iowait = 0;  // MUST BE INITIALIZED
+   
+   float Prev_Active_Total = 0;  // MUST BE INITIALIZED
+   float Prev_User = 0;  // MUST BE INITIALIZED
+   float Prev_Nice = 0;  // MUST BE INITIALIZED
+   float Prev_System = 0;  // MUST BE INITIALIZED
+   float Prev_Irq = 0;  // MUST BE INITIALIZED
+   float Prev_Softirq = 0;  // MUST BE INITIALIZED
+   float Prev_Steal = 0;  // MUST BE INITIALIZED
+   
+   float Prev_Total = 0;  // MUST BE INITIALIZED
+   
 };
 
 #endif
--- CppND-System-Monitor-Project-Updated/src/processor.cpp
+++ CppND-System-Monitor-Project-Updated/src/processor.cpp
@@ -10,17 +10,27 @@ using namespace std;
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization()
 {
-    long total_jiffies = LinuxParser::Jiffies();
-    long active_jiffies = LinuxParser::ActiveJiffies();
-    
-    float current_total_time = total_jiffies;   // change long to float
-    float current_active_time = active_jiffies;   // change long to float
+    // CANNOT use "LinuxParser::Jiffies() & LinuxParser::ActiveJiffies()" because Previous values are needed
 
-    float total_time = current_total_time - prev_total_time;
-    float active_time = current_active_time - prev_active_time;
-
-    float prev_total_time = total_time;    // make Current total time, Previous total time
-    float prev_active_time = active_time;    // make Current active time, Previous active time
+    Prev_Idle_Total = Prev_Idle + Prev_Iowait;
+    float current_Idle = LinuxParser::kIdle_ + LinuxParser::kIOwait_;
+    float Prev_Idle = LinuxParser::kIdle_;    // make Current active time, Previous active time
+    float Prev_Iowait = LinuxParser::kIOwait_;    // make Current active time, Previous active time
     
-    return (active_time / total_time) * total_time;     // CPU usage percent = (Active_CPU_time / Total_CPU_time) * 100         total_time = 100%
+    Prev_Active_Total = Prev_User + Prev_Nice + Prev_System + Prev_Irq + Prev_Softirq + Prev_Steal;
+    float current_Active = LinuxParser::kUser_ + LinuxParser::kNice_ + LinuxParser::kSystem_ + LinuxParser::kIRQ_ + LinuxParser::kSoftIRQ_ + LinuxParser::kSteal_;
+    float Prev_User = LinuxParser::kUser_;    // make Current active time, Previous active time
+    float Prev_Nice = LinuxParser::kNice_;    // make Current total time, Previous total time
+    float Prev_System = LinuxParser::kSystem_;    // make Current total time, Previous total time
+    float Prev_Irq = LinuxParser::kIRQ_;    // make Current total time, Previous total time
+    float Prev_Softirq = LinuxParser::kSoftIRQ_;    // make Current total time, Previous total time
+    float Prev_Steal = LinuxParser::kSteal_;    // make Current total time, Previous total time
+
+    Prev_Total = Prev_Idle_Total + Prev_Active_Total;
+    float current_Total = current_Active - current_Idle;
+
+    float Total = current_Total - Prev_Total;
+    float Idle = current_Idle - Prev_Idle_Total;
+    
+    return (Total - Idle) / Total;     // CPU usage percent = (Total_CPU_time - Idle_CPU_time) / Total_CPU_time
 }
