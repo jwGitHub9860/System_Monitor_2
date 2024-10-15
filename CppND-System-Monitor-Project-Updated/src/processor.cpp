8487675083907e8f120ae7da46edc268b8a5829a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-15 19:59:42 -0400

    Delete "float" typename for private variables in processor.cpp

--- CppND-System-Monitor-Project-Updated/src/processor.cpp
+++ CppND-System-Monitor-Project-Updated/src/processor.cpp
@@ -12,17 +12,17 @@ float Processor::Utilization()
 
     Prev_Idle_Total = Prev_Idle + Prev_Iowait;
     float current_Idle = LinuxParser::kIdle_ + LinuxParser::kIOwait_;
-    float Prev_Idle = LinuxParser::kIdle_;    // make Current active time, Previous active time
-    float Prev_Iowait = LinuxParser::kIOwait_;    // make Current active time, Previous active time
+    Prev_Idle = LinuxParser::kIdle_;    // make Current active time, Previous active time
+    Prev_Iowait = LinuxParser::kIOwait_;    // make Current active time, Previous active time
     
     Prev_Active_Total = Prev_User + Prev_Nice + Prev_System + Prev_Irq + Prev_Softirq + Prev_Steal;
     float current_Active = LinuxParser::kUser_ + LinuxParser::kNice_ + LinuxParser::kSystem_ + LinuxParser::kIRQ_ + LinuxParser::kSoftIRQ_ + LinuxParser::kSteal_;
-    float Prev_User = LinuxParser::kUser_;    // make Current active time, Previous active time
-    float Prev_Nice = LinuxParser::kNice_;    // make Current total time, Previous total time
-    float Prev_System = LinuxParser::kSystem_;    // make Current total time, Previous total time
-    float Prev_Irq = LinuxParser::kIRQ_;    // make Current total time, Previous total time
-    float Prev_Softirq = LinuxParser::kSoftIRQ_;    // make Current total time, Previous total time
-    float Prev_Steal = LinuxParser::kSteal_;    // make Current total time, Previous total time
+    Prev_User = LinuxParser::kUser_;    // make Current active time, Previous active time
+    Prev_Nice = LinuxParser::kNice_;    // make Current total time, Previous total time
+    Prev_System = LinuxParser::kSystem_;    // make Current total time, Previous total time
+    Prev_Irq = LinuxParser::kIRQ_;    // make Current total time, Previous total time
+    Prev_Softirq = LinuxParser::kSoftIRQ_;    // make Current total time, Previous total time
+    Prev_Steal = LinuxParser::kSteal_;    // make Current total time, Previous total time
 
     Prev_Total = Prev_Idle_Total + Prev_Active_Total;
     float current_Total = current_Active - current_Idle;
